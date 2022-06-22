256613fc63fb6d8bbea136889ada5ea6c87cb4d2
rocksayush <64919132+rocksayush@users.noreply.github.com>
2022-06-22 15:51:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ rclone() {
   apt update && sudo apt install fuse
   sed -i '/#user/s/#//g' /etc/fuse.conf
   mkdir /mnt/media
-  clone config
+  rclone config
 }
 # ★★★Mounting using rclone★★★
 mount() {
@@ -35,60 +35,10 @@ mount() {
   crontab mycron
   rm mycron
 } 
-# ★★★Installation of Plex★★★
-plex () {
-  sudo apt-get update
-  sudo apt-get upgrade
-  wget https://downloads.plex.tv/plex-media-server-new/1.20.1.3252-a78fef9a9/debian/plexmediaserver_1.20.1.3252-a78fef9a9_amd64.deb
-  sudo dpkg -i plexmediaserver*.deb
-  sudo systemctl enable plexmediaserver.service
-  sudo systemctl start plexmediaserver.service
-  echo "${BLUE}Plex has been Installed succesfully${NORMAL}"
-  echo "${RED}Go to ip.adress.of.server:32400 in your browser now${NORMAL}"
-}
-# ★★★Installation of Emby★★★
-emby() {
-  sudo apt-get update
-  sudo apt-get upgrade
-  wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.5.1.0/emby-server-deb_4.5.1.0_amd64.deb
-  sudo dpkg -i emby-server-*.deb
-  sudo service emby-server start
-  echo "${BLUE}Emby has been Installed succesfully${NORMAL}"
-  echo "${RED}Go to ip.adress.of.server:8096 in your browser now${NORMAL}"
-}
-# ★★★Installation of Jellyfin★★★
-jellyfin() {
-  sudo apt-get update
-  sudo apt-get upgrade
-  sudo apt install apt-transport-https
-  sudo add-apt-repository universe
-  wget -O - https://repo.jellyfin.org/ubuntu/jellyfin_team.gpg.key | sudo apt-key add -
-  sudo touch /etc/apt/sources.list.d/jellyfin.list
-  echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
-  sudo apt update
-  sudo apt install jellyfin
-  sudo systemctl start jellyfin.service
-  echo "${BLUE}Jellyfin has been Installed succesfully${NORMAL}"
-  echo "${RED}Go to ip.adress.of.server:8096 in your browser now${NORMAL}"
-}
-
-# ★★★Installation★★★
-echo && echo " ${BLUE}Media Server Installation Srcipt${NORMAL} by ${RED}Roshanconnor${NORMAL}
-
-➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
-1.${GREEN}Setting Up Plex with Gdrive${NORMAL}
-2.${GREEN}Setting Up Emby with Gdrive${NORMAL}
-3.${GREEN}Setting Up Jellyfin with Gdrive${NORMAL}
-➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
-69.${PURPLE}Exit${NORMAL}" && echo
-
 
     rclone
     mount
     #jellyfin	
     ;;	
-    echo
-    echo " ${RED}Choose Correct Number from the Options${NORMAL}"
-    ;;
 esac 
   
